8b51b31a1a2e723f0dde2dcdf4f9be2c16de2f18
Rakotojaona Nambinina <104864333+AndrianDevFPGA@users.noreply.github.com>
2023-04-15 18:28:44 +0200

    Update trigger.v

--- trigger.v
+++ trigger.v
@@ -39,3 +39,43 @@ module trigger(
   end 
        
 endmodule
+
+/*
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+
+entity triggerSignal is
+    port (
+        clk, rst: in std_logic; 
+        tx: out std_logic
+    );
+end entity;
+
+architecture Behavioral of triggerSignal is
+
+    signal counter : unsigned(6 downto 0);
+
+begin
+
+    process (clk) is
+    begin
+        if rising_edge(clk) then
+            if rst = '1' then
+                tx <= '0';
+                counter <= (others => '0');
+            else
+                counter <= counter + 1;
+                if counter = "1100100" then
+                    tx <= '1';
+                    counter <= (others => '0');
+                else
+                    tx <= '0';
+                end if;
+            end if;
+        end if;
+    end process;
+
+end architecture;
+
+*/
